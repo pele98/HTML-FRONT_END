194ca1d5ed3b58127b382773219b9c8dabbddd3b
JarkkoJake <jarkkom.heinonen@gmail.com>
2021-04-26 14:47:50 +0300

    main js now adds icon to the tab on every page

--- Frontend_project/main.js
+++ Frontend_project/main.js
@@ -5,6 +5,9 @@ var current_site_button = document
   .getElementsByClassName("nav_current")[0];
 current_site_button.style.borderStyle = "dashed";
 
+// Sets icon to the tab
+document.getElementsByTagName("head")[0].innerHTML += 	"<link rel='icon' href='images/logo.png'>"
+
 function starmanURL() {
   opener.location = "https://www.google.com";
 }
